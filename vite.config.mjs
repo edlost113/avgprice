b0c5c86736471baf9ef56dfe368b481625da1e8c
edlost113 <pattis78@gmail.com>
2025-02-28 18:31:07 -0500

    only display errors in console?

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,6 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: '/avgprice/',
+  logLevel: 'error',
   test: {
     globals: true,
     environment: 'jsdom',
